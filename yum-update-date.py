20c673fc93726db66ed2dbf0fd401c3d73749732
Sreeraj M S <sreewave@gmail.com>
2019-02-18 10:52:32 +0530

    Update yum-update-date.py
    Added docstrings, authorship and version information.

--- yum-update-date.py
+++ yum-update-date.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+"""To query the yum database and fetch the last time when a yum update all was run"""
+__author__      =       "Sreeraj M S"
+__email__       =       "sreewave@gmail.com"
+__version__     =       "1.0"
+
 import os
 import datetime
 import sqlite3
@@ -11,8 +16,7 @@ for yumDBfile in os.listdir(dbdir):
 
 conn = sqlite3.connect(DBfile)
 cur = conn.cursor()
-cur.execute("select timestamp from trans_beg where tid=(select tid from trans_cmdline where cmdline='update -y' OR cmdline='update' order by tid desc limit 1)")
+cur.execute("select timestamp from trans_beg where tid=(select tid from trans_cmdline where cmdline='update -y' OR cmdline='update' OR cmdline='update all' order by tid desc limit 1)")
 patchtime = cur.fetchall()
 print datetime.datetime.fromtimestamp(patchtime[0][0]).strftime('%c')
-
  
